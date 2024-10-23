cecb4b924bbd6f3b5b2853e1ecc18a86282521da
Breno Oliveira Dias <bre.oliveira.dias@gmail.com>
2024-10-23 18:04:10 -0300

    [WIP] Rework do contador

--- Assets/Scripts/UI/HUD/ScoreUI.cs
+++ Assets/Scripts/UI/HUD/ScoreUI.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -8,16 +10,26 @@ public class ScoreUI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     TextMeshProUGUI _revengeText;
+    float _revengeFontSize;
+    Color _revengeFontColor;
     int _currentScore;
     
     private void Awake()
     {
         _revengeText = GetComponent<TextMeshProUGUI>();
+        _revengeFontSize = _revengeText.fontSize;
+        _revengeFontColor = _revengeText.color;
     }
 
-    public void UpdateScore()
+    public async void  UpdateScore()
     {
         _currentScore++;
         _revengeText.text = $"{_currentScore.ToString("D4")}";
+        _revengeText.color = Color.yellow;
+        _revengeText.fontSize = _revengeFontSize * 1.1f;
+        await Task.Delay(500);
+        _revengeText.fontSize = _revengeFontSize;
+        _revengeText.color = _revengeFontColor;
+        
     }
 }
