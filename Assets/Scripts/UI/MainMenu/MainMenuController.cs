2841b5d8f55335146a86a4fe7fd09dae39ef5198
Breno Oliveira Dias <bre.oliveira.dias@gmail.com>
2024-10-23 01:30:58 -0300

    [WIP] Main Menu

---
+++ Assets/Scripts/UI/MainMenu/MainMenuController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public void IniciarJogo()
+    {
+        SceneManager.LoadScene("Diogo");
+    }
+
+}
