17874c6df286649b949c56be8291ff6a051203f2
Breno Oliveira Dias <bre.oliveira.dias@gmail.com>
2024-11-17 21:36:33 -0300

    [ADD] Health Bar

---
+++ Assets/Scripts/UI/HUD/HealthBar.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider RedBarSlider;
+    public Slider YellowBarSlider;
+    public Adventurer adventurer;
+
+    [SerializeField] public float lerpSpeed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Pegando vida máxima do player
+        adventurer = FindFirstObjectByType<Adventurer>();
+        RedBarSlider.maxValue = adventurer.life;
+        YellowBarSlider.maxValue = adventurer.life;
+        RedBarSlider.value = adventurer.life;
+        YellowBarSlider.value = adventurer.life;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (RedBarSlider.value != adventurer.life)
+        {
+            RedBarSlider.value = adventurer.life;
+        }
+
+        if (RedBarSlider.value != YellowBarSlider.value)
+        {
+            YellowBarSlider.value = Mathf.Lerp(YellowBarSlider.value, adventurer.life, lerpSpeed);
+        }
+    }
+}
