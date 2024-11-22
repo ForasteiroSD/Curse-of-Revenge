69502cc1b66229429ec0b2d4cbf7b9b760f7d7dd
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2024-11-22 00:44:05 -0300

    Change Enemies Feet Collider

--- Assets/Scripts/Enemies/EnemiesScript.cs
+++ Assets/Scripts/Enemies/EnemiesScript.cs
@@ -5,41 +5,38 @@ using Utils;
 
 public class EnemiesScript : MonoBehaviour
 {
+    //Referencies
     protected Animator _animator;
-    
     protected Rigidbody2D _rb;
-
-    [SerializeField] public float _horSpeed = 2f;
-    
-    [SerializeField] protected float _idleTime = 2f;
-    protected bool _idle = false;
-
-    protected bool _isChasing = false;
-    [SerializeField] protected float _chaseSpeedMultiplier = 1.1f;
-
     protected Transform _player;
     [SerializeField] public Transform _maxChasePos;
     [SerializeField] public Transform _minChasePos;
+    [SerializeField] private GameObject _revengePoint;
+    [SerializeField] protected GameObject _textDamage;
 
+
+    //Status
+    [SerializeField] public float _health = 20f;
+    [SerializeField] public float _horSpeed = 2f;
+    [SerializeField] protected float _idleTime = 2f;
+    [SerializeField] protected float _chaseSpeedMultiplier = 1.1f;
     [SerializeField] protected float _attackDistance = 1.5f;
     [SerializeField] protected float _attackCooldown = 1.5f;
     [SerializeField] protected float _attackDamage = 1f;
     [SerializeField] protected float _damageReceivedMult = 0.8f;
-    protected int _isAttacking = 1;
-
-    [SerializeField] public float _health = 20f;
     [SerializeField] protected float _hitDelay = 1f;
-    protected bool _hit = false;
-
-    protected bool _death = false;
-
     [SerializeField] protected int _valuePerRevengePoint = 1;
     [SerializeField] protected int _revengePointsQuantity = 1;
-
-    [SerializeField] private GameObject _revengePoint;
-    [SerializeField] protected GameObject _textDamage;
-
     [SerializeField] protected float _move = 0.2f;
+
+    //States
+    protected bool _idle = false;
+    protected bool _isChasing = false;
+    protected int _isAttacking = 1;
+    protected bool _hit = false;
+    protected bool _death = false;
+    protected bool _isOnBorder = false;
+    protected bool _attackedOnBorder = false;
 
     protected virtual void Awake()
     {
@@ -51,6 +48,13 @@ public class EnemiesScript : MonoBehaviour
     protected virtual void Update()
     {
         if (_isAttacking == 0 || _hit || _death) return;
+
+        if (_attackedOnBorder && _isAttacking == 1)
+        {
+            StartCoroutine(Idle());
+            _attackedOnBorder = false;
+            return;
+        }
 
         if (_isChasing)
         {
@@ -96,32 +100,30 @@ public class EnemiesScript : MonoBehaviour
             return;
         }
 
-
-        if (_isAttacking == 1 && !_hit && !_death)
+        if(_isOnBorder && _isAttacking == 1 && !_hit && !_death)
         {
-            if (transform.position.x <= _minChasePos.position.x + 0.5)
+            float distanceMax = _maxChasePos.position.x - transform.position.x;
+            float distanceMin = transform.position.x - _minChasePos.position.x;
+            int border = distanceMax > distanceMin ? 0 : 1;
+            if (border == 0 && _player.position.x <= _minChasePos.position.x && _player.position.x >= _minChasePos.position.x - 1)
             {
-                if (_player.position.x <= _minChasePos.position.x && _player.position.x >= _minChasePos.position.x - _attackDistance)
-                {
-                    float distance = _player.transform.position.x - transform.position.x;
+                float distance = _player.transform.position.x - transform.position.x;
 
-                    if(distance >= 0 && _horSpeed < 0) Flip();
-                    else if(distance < 0 && _horSpeed > 0) Flip();
+                if(distance >= 0 && _horSpeed < 0) Flip();
+                else if(distance < 0 && _horSpeed > 0) Flip();
 
-                    StartCoroutine(Attack());
-                }
+                StartCoroutine(Attack());
+                _attackedOnBorder = true;
             }
-            else if (transform.position.x >= _maxChasePos.position.x - 0.5)
+            else if (border == 1 && _player.position.x >= _maxChasePos.position.x && _player.position.x <= _maxChasePos.position.x + 1)
             {
-                if (_player.position.x >= _maxChasePos.position.x && _player.position.x <= _maxChasePos.position.x + _attackDistance)
-                {
-                    float distance = _player.transform.position.x - transform.position.x;
+                float distance = _player.transform.position.x - transform.position.x;
 
-                    if(distance >= 0 && _horSpeed < 0) Flip();
-                    else if(distance < 0 && _horSpeed > 0) Flip();
+                if(distance >= 0 && _horSpeed < 0) Flip();
+                else if(distance < 0 && _horSpeed > 0) Flip();
 
-                    StartCoroutine(Attack());
-                }
+                StartCoroutine(Attack());
+                _attackedOnBorder = true;
             }
         }
 
@@ -216,12 +218,14 @@ public class EnemiesScript : MonoBehaviour
     {
         _rb.linearVelocityX = 0;
         _idle = true;
+        _isOnBorder = true;
         _animator.SetBool(Constants.IDLE_ENEMY, true);
 
         yield return new WaitForSeconds(_idleTime);
 
-        if (_isChasing) yield break;
+        if (_isChasing || _isAttacking == 0) yield break;
 
+        _isOnBorder = false;
         _idle = false;
         _animator.SetBool(Constants.IDLE_ENEMY, false);
         Flip();
